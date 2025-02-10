d87037a98c49be1cb21f4c04553007c6c88b0092
CristianoDinh <dinhgiabao1142004@gmail.com>
2025-02-10 17:22:10 +0700

    update url code

--- source-code/url.py
+++ source-code/url.py
@@ -1,5 +1,26 @@
 from argparse import ArgumentParser, Namespace
-import boto3
+import boto3, os
+from dotenv import load_dotenv
+
+#=====================================================================
+# Load file .env
+load_dotenv()
+
+# Read parameter from .env (environment variable)
+ENDPOINT_URL = os.getenv('S3_ENDPOINT')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_REGION = os.getenv('REGION_NAME')
+
+# Initialize s3 client with Endpoint and Authenticate
+s3 = boto3.client(
+  's3',
+        endpoint_url=ENDPOINT_URL,
+        aws_access_key_id = AWS_ACCESS_KEY_ID,
+        aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
+        region_name=AWS_REGION
+)
+#=====================================================================
 
 parser = ArgumentParser()
 #parser.usage
@@ -12,7 +33,6 @@ parser.add_argument("file_name", help = ":File needs presigning name", type = st
 parser.add_argument("expires_in", help = ":Expiration time (in seconds)", type = int)
 
 args : Namespace = parser.parse_args()
-s3 = boto3.client('s3', region_name='ap-southeast-1')
 
 BUCKET_SOURCE = args.bucket_source_name
 FILE_NAME = args.file_name
