c24cab3ba5783b0bd17c619b485ac87d37d38467
CristianoDinh <dinhgiabao1142004@gmail.com>
2025-02-11 15:45:51 +0700

    update code

--- source-code/url.py
+++ source-code/url.py
@@ -14,7 +14,7 @@ AWS_REGION = os.getenv('REGION_NAME')
 
 # Initialize s3 client with Endpoint and Authenticate
 s3 = boto3.client(
-  's3',
+        's3',
         endpoint_url=ENDPOINT_URL,
         aws_access_key_id = AWS_ACCESS_KEY_ID,
         aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
