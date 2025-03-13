a17800984900a966af15eb19cedc6cefb936c61f
CristianoDinh <dinhgiabao1142004@gmail.com>
2025-03-13 11:26:50 +0700

    add env for development & production

---
+++ src-ui/src/environments/environment.production.ts
@@ -0,0 +1,5 @@
+// Environment variables of Production environments.
+export const environment = {
+  production: true,
+  apiUrl: `https://interdata-cli-api.vercel.app`
+};
---
+++ src-ui/src/environments/environment.ts
@@ -0,0 +1,4 @@
+// The base file environment.ts, contains the default environment settings. For example:
+export const environment = {
+  production: true,
+};
